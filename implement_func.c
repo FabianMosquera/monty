3b004cf183773943c395680f2320cb636ecf7bd9
FabianMosquera <rfmq9530@gmail.com>
2020-05-13 23:14:26 +0000

    input

---
+++ implement_func.c
@@ -0,0 +1,23 @@
+#include "monty.h"
+/**
+ * implement_func - get the option input
+ * @str: input opcode
+ * Return: stack
+ */
+void (*implement_func(char *str))(stack_t **head, unsigned int line_n)
+{
+	int i = 0;
+	instruction_t stack[] = {
+		{"push", stack_push},
+		/*{"pall", stack_pall},*/
+		{NULL, NULL}
+	};
+
+	while (stack[i].opcode)
+	{
+		if (strcmp(stack[i].opcode, str) == 0)
+			break;
+		i++;
+	}
+	return (stack[i].f);
+}
