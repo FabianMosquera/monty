26d9be3c30ee72634e2bdda7c1cb233b22946da3
FabianMosquera <rfmq9530@gmail.com>
2020-05-14 22:10:01 +0000

    fix the compilation

--- more_functions.c
+++ more_functions.c
@@ -55,7 +55,7 @@ void stack_sub(stack_t **head, unsigned int line_n)
 	if (i < 2)
 	{
 		dprintf(2, "L%u: can't sub, stack too short\n", line_n);
-		free_vars();
+		free_var();
 		exit(EXIT_FAILURE);
 	}
 	tmp = (*head)->next;
@@ -82,13 +82,13 @@ void stack_div(stack_t **head, unsigned int line_n)
 	if (i < 2)
 	{
 		dprintf(2, "L%u: can't div, stack too short\n", line_n);
-		free_vars();
+		free_var();
 		exit(EXIT_FAILURE);
 	}
 	if ((*head)->n == 0)
 	{
 		dprintf(2, "L%d: division by zero\n", line_n);
-		free_vars();
+		free_var();
 		exit(EXIT_FAILURE);
 	}
 	tmp = (*head)->next;
@@ -114,7 +114,7 @@ void stack_mul(stack_t **head, unsigned int line_n)
 	if (i < 2)
 	{
 		dprintf(2, "L%u: can't mul, stack too short\n", line_n);
-		free_vars();
+		free_var();
 		exit(EXIT_FAILURE);
 	}
 	tmp = (*head)->next;
