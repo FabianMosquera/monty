96e1ecf25c16d49b1e2792b9fb4c215d0e1a8357
FabianMosquera <rfmq9530@gmail.com>
2020-05-14 22:03:53 +0000

    fix betty

--- implement_func.c
+++ implement_func.c
@@ -12,12 +12,12 @@ void (*implement_func(char *str))(stack_t **head, unsigned int line_n)
 		{"pall", stack_pall},
 		{"pint", stack_pint},
 		{"pop", stack_pop},
-		{"swap",stack_swap},
-		{"add",stack_add},
-		{"nop",stack_nop},
-		{"sub",stack_sub},
-		{"div",stack_div},
-		{"mul",stack_mul},
+		{"swap", stack_swap},
+		{"add", stack_add},
+		{"nop", stack_nop},
+		{"sub", stack_sub},
+		{"div", stack_div},
+		{"mul", stack_mul},
 		{NULL, NULL}
 	};
 
