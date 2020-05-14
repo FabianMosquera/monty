254862ef35fc24c017fde553e2fc675d83bc2e82
FabianMosquera <rfmq9530@gmail.com>
2020-05-14 16:08:44 +0000

    add more functions

--- functions.c
+++ functions.c
@@ -66,3 +66,52 @@ void stack_pint(stack_t **head, unsigned int line_n)
 	}
 	printf("%d\n", (*head)->n);
 }
+/**
+ * stack_pop - removethe top element
+ * @head: head of doubly linked list
+ * @line_n: Reference to the line of doubly linked list
+ */
+void stack_pop(stack_t **head, unsigned int line_n)
+{
+	stack_t *tmp;
+
+	if (head == NULL || *head == NULL)
+	{
+		dprintf(2, "L%u: can't pop an empty stack\n", line_n);
+		free_var();
+		exit(EXIT_FAILURE);
+	}
+	tmp = *head;
+	*head = (*head)->next;
+	free(tmp);
+}
+/**
+ * stack_swap - sawp the two top element
+ * @head: head of doubly linked list
+ * @line_n: Reference to the line of doubly linked list
+ */
+void stack_swap(stack_t **head, unsigned int line_n)
+{
+	int i = 0;
+	stack_t *tmp = NULL;
+
+	tmp = *head;
+
+	while (tmp != NULL)
+	{
+		i++;
+		tmp = tmp->next;
+	}
+	if (i < 2)
+	{
+		dprintf(2, "L%u: can't swap, stack too short\n", line_n);
+		free_var();
+		exit(EXIT_FAILURE);
+	}
+	tmp = *head;
+	*head = (*head)->next;
+	tmp->next = (*head)->next;
+	tmp->prev = *head;
+	(*head)->next = tmp;
+	(*head)->prev = NULL;
+}
